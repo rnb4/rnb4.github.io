fb753b5dde34043c015ee3e0d5f5b6a007dd1489
poignantbanana <github.flatly275@passmail.net>
2025-11-14 17:40:28 -0500

    mahjong puzzle v0.6

--- mahjong-puzzle/index.service.worker.js
+++ mahjong-puzzle/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1763147632|45447173283';
+const CACHE_VERSION = '1763159998|57812397037';
 /** @type {string} */
 const CACHE_PREFIX = 'Majong Puzzle-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
